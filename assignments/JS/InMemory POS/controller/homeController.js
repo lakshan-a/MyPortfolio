177e7865a487e97f90ea1c616d13826c680473de
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2023-10-07 19:36:58 +0530

    updated

--- assignments/JS/InMemory POS/controller/homeController.js
+++ assignments/JS/InMemory POS/controller/homeController.js
@@ -34,18 +34,18 @@ document.getElementById("btnOrder").addEventListener("click",function (){
     document.getElementById("itemPage").style.display="none";
     document.getElementById("orderPage").style.display="block";
     document.getElementById("allOrdersPage").style.display="none";
-    loadCusIds();
+    // loadCusIds();
     loadItemIds();
     generateOrderId();
 });
 
 
-document.getElementById("allOders").addEventListener("click",function (){
+document.getElementById("btnOrderDetail").addEventListener("click",function (){
     document.getElementById("homePage").style.display="none";
     document.getElementById("customerPage").style.display="none";
     document.getElementById("itemPage").style.display="none";
     document.getElementById("orderPage").style.display="none";
     document.getElementById("allOrdersPage").style.display="block";
-    allOrders();
+    // allOrders();
 });
 
